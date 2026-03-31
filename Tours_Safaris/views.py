604c4ffb28bcfa72a09869f52d86c5c42412300c
Bseries Bseries <bseriescompany@gmail.com>
2026-03-31 16:45:23 +0300

    Fix settings for server deployment

--- Tours_Safaris/views.py
+++ Tours_Safaris/views.py
@@ -234,10 +234,3 @@ def test_contacts_api(request):
     
 
 
-#For production
-from django.views.generic import TemplateView
-from django.urls import path
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name="index.html")),
-]
