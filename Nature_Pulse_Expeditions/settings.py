15a51e5399b562c5aa389e57086255401c01cc6a
Bseries Bseries <bseriescompany@gmail.com>
2026-04-01 23:24:18 +0300

    Updated requirements and deployment fixes

--- Nature_Pulse_Expeditions/settings.py
+++ Nature_Pulse_Expeditions/settings.py
@@ -148,9 +148,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # settings.py
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static'),
-#]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # include React assets
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # collected static files
 
