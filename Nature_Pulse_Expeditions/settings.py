a0bfa4556f9b401b52333f4997bf8d29813e263d
Bseries Bseries <bseriescompany@gmail.com>
2026-04-01 23:27:03 +0300

    Updated requirements and deployment fixes

--- Nature_Pulse_Expeditions/settings.py
+++ Nature_Pulse_Expeditions/settings.py
@@ -145,14 +145,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# settings.py
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # include React assets
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # collected static files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 
 # Media files (uploaded images etc.)
 MEDIA_URL = '/media/'
